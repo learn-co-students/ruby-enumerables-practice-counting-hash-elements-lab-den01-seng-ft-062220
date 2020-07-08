32f2272157278537d8a1c6bc8ccc4bfab89b41f6
Gerald Bivens <geraldbivens@gmail.com>
2020-07-08 15:04:20 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,10 +1,18 @@
-def count_elements(array)
+require 'pry'
+
+def count_elements(animals)
   new_hash = {}
-    array.count do |element|
-      new_hash << element
-      if element == new_hash[key]
-        element += 1
-      else
-      end
+  
+  animals.each do |animal|
+    if new_hash[animal] # this returns nil for the value, so it skips to else
+      new_hash[animal] += 1 # short hand version
+      # new_hash[animal] = new_hash[animal] + 1 long hand version
+    else
+      new_hash[animal] = 1
+    end
+  end 
+
   new_hash
 end
+
+#=> {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
