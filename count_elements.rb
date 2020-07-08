addd10c939cb64ae6de343ee6b5fe2f535d189b2
Gerald Bivens <geraldbivens@gmail.com>
2020-07-08 13:02:55 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,10 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+    array.count do |element|
+      new_hash << element
+      if element == new_hash[key]
+        element += 1
+      else
+      end
+  new_hash
 end
- 
